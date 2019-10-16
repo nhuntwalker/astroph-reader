6524bc63ccf799a0f1ee22a01e2d4bd25774ed0f
nhuntwalker <nhuntwalker@Gmail.com>
2019-10-16 11:37:02 -0700

    Added a rough HTML scraper for the astroPH-new page

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as soup
+
+ASTROPH_URL = "https://arxiv.org/list/astro-ph/new"
+
+response = requests.get(ASTROPH_URL)
+response = requests.get(ASTROPH_URL)
+html = soup(response.content, "html.parser")
+
+new_articles = html.dl.find_all('dd')
+
+formatted_articles = []
+
+for article in new_articles:
+    title = article.find('div', attrs={'class': 'list-title'}).text.strip().replace('Title: ', '')
+    abstract = article.find('p', attrs={'class': 'mathjax'}).text.strip().replace('\n', ' ')
+    formatted_article = {
+        "title": title,
+        "abstract": abstract
+    }
+    formatted_articles.append(formatted_article)
+
+print(formatted_articles)
